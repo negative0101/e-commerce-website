5acdc377bfcb3bed7caaa0262308a905cd766674
negative0101 <negativeoxide@gmail.com>
2022-04-06 22:43:48 +0300

    fixed allowed hosts

--- multi_vendor/multi_vendor/settings.py
+++ multi_vendor/multi_vendor/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'django-insecure-nz!c_oe3n7c^cm@7s%7tt8qd*p3$4l0aixdop2i#woicqww710
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1',
-                 'bigblackpussy.herokuapp.com', ]
+                 'bigblackcat.herokuapp.com', ]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
